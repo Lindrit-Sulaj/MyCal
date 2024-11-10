fdf1e683c983b85bb5acf9f2019c04e9addf8a8e
Lindrit <lindritsulaj2.0@gmail.com>
2024-11-10 16:41:26 +0100

    no checks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,8 @@ const handler = NextAuth({
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      checks: ['none']
     }),
     Credentials({
       id: 'credentials',
